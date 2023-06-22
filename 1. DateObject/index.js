cb64117119c20c0912ca67cc0dfb501741b59e81
Indrajit-Das <75366074+Indrajit-Das@users.noreply.github.com>
2023-06-22 20:46:31 +0600

    Update index.js

--- 1. DateObject/index.js
+++ 1. DateObject/index.js
@@ -1,9 +1,9 @@
 var getCurrentDay=(myDate)=>{
     var days =["Sunday"," Monday"," Tuesday", "Wednesday" ,"Thursday", "Friday","Saturday"];
-    var obj = new Date();
+    var obj = new Date(myDate);
     // here obj.getDay(myDate) will return the numeric value 
     // from the days array index we can get the name of the day 
-    var getCurrentDay =days[obj.getDay(myDate)];
+    var getCurrentDay =days[obj.getDay()];
     return getCurrentDay;
 }
 console.log("The day of the week is : "+getCurrentDay("2023-06-19"));
